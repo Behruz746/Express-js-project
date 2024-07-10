5a9db9402f90a43675c3f394381019bf75fcb699
Behruz <madaminovbehruz746@gmail.com>
2024-07-10 13:55:02 +0500

    added: mongos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 import { create } from "express-handlebars";
+import * as dotenv from "dotenv";
+// Routes
 import AutherRouters from "./routes/auth.js";
 import ProductRouters from "./routes/products.js";
+dotenv.config();
 
 // express methodlarini saqlovchiga olish
 const app = express();
@@ -14,14 +18,28 @@ const hbs = create({
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-app.set("viewa", "./views");
+app.set("views", "./views"); // Typo correction here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
 app.use(AutherRouters);
 app.use(ProductRouters);
 
-const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+const startApp = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () =>
+      console.log("Mongo DB Connacted")
+    );
+
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
