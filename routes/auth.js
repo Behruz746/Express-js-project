b767356f42da6890ae7915914075bdc07023af03
Behruz <madaminovbehruz746@gmail.com>
2024-07-10 14:17:24 +0500

    added: model mongoose, User data

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import User from "../models/User.js";
 const router = Router();
 
 router.get("/login", (req, res) => {
@@ -21,9 +22,18 @@ router.post("/login", (req, res) => {
   res.redirect("/");
 });
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
+router.post("/register", async (req, res) => {
   // login bo'lgandan so'ng home pagega userni o'tqizvoradi   res.redirect("/")
+  const userData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  // console.log(req.body);
+  // console.log(userData);
+  const user = await User.create(userData);
+  console.log(user);
   res.redirect("/");
 });
 
