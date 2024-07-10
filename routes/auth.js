d865da7e969f2113fce57e33d18b49f18fc1e264
Behruz <madaminovbehruz746@gmail.com>
2024-07-10 14:55:52 +0500

    added: login api handler

--- routes/auth.js
+++ routes/auth.js
@@ -17,10 +17,34 @@ router.get("/register", (req, res) => {
   });
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.body);
-  // login bo'lgandan so'ng home pagega userni o'tqizvoradi   res.redirect("/")
-  res.redirect("/");
+router.post("/login", async (req, res) => {
+  try {
+    // hozirga email bilan serverdagi emailni solish tirish
+    const existUser = await User.findOne({ email: req.body.email });
+    // agar email false bolsa error ishlasin
+    if (!existUser) {
+      throw new Error("User not found");
+      return false;
+    }
+
+    // unday bo'lmasa passwordlarni solishtirsin
+    const isPassEqual = await bcrypt.compare(
+      req.body.password,
+      existUser.password
+    );
+    // password false bo'lsa error ishlasin
+    if (!isPassEqual) {
+      throw new Error("Password Wrong");
+      return false;
+    }
+
+    console.log(existUser);
+    // login bo'lgandan so'ng home pagega userni o'tqizvoradi   res.redirect("/")
+    res.redirect("/");
+  } catch (error) {
+    console.error("Userni topishda xato yuz berdi:", error);
+    res.status(500).send("Userni topishda xato yuz berdi");
+  }
 });
 
 router.post("/register", async (req, res) => {
@@ -36,8 +60,7 @@ router.post("/register", async (req, res) => {
       // hashlangan password
       password: hashedPassword,
     };
-    // console.log(req.body);
-    // console.log(userData);
+
     const user = await User.create(userData);
     console.log(user);
     res.redirect("/");
