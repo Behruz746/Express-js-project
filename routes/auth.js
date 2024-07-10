2e42b83b61e67e407672fbc7031779a252837164
Behruz <madaminovbehruz746@gmail.com>
2024-07-10 14:38:12 +0500

    added: hashed password with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 const router = Router();
 
 router.get("/login", (req, res) => {
@@ -23,18 +24,27 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-  // login bo'lgandan so'ng home pagega userni o'tqizvoradi   res.redirect("/")
-  const userData = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  // console.log(req.body);
-  // console.log(userData);
-  const user = await User.create(userData);
-  console.log(user);
-  res.redirect("/");
+  try {
+    // bcrypt yordamidan user passwordlarini hash qilsh
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // login bo'lgandan so'ng home pagega userni o'tqizvoradi   res.redirect("/")
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      // hashlangan password
+      password: hashedPassword,
+    };
+    // console.log(req.body);
+    // console.log(userData);
+    const user = await User.create(userData);
+    console.log(user);
+    res.redirect("/");
+  } catch (error) {
+    console.error("User yaratishda xato yuz berdi:", error);
+    res.status(500).send("User yaratishda xato yuz berdi");
+  }
 });
 
 export default router;
